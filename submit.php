1eed0b95f88bf823ba9ba4b363e4d29cffed806f
Makesi Fortune <wizmanjohn>
2019-06-01 17:46:05 -0400

    first commit

---
+++ submit.php
@@ -0,0 +1,10 @@
+<<?php 
+	data_file = fopen("text.txt", mode)
+
+	name = /action_page.php["name"];
+	lastname = /action_page.php["lastname"];
+	text_to_write = name . " " . lastname;
+
+	fwrite(data_file, string);
+	fclose(data_file);
+?>
